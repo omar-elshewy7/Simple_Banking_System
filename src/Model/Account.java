13ea46ef8b2295e61a4ad4b93d7459b884a6094f
Omar_El-Shewy <moryelshewy@outlook.com>
2024-08-30 01:46:58 +0300

    Created an account.java file

---
+++ src/Model/Account.java
@@ -0,0 +1,35 @@
+package Model;
+
+public class Accoount {
+    private String name;
+    private String phoneNumber;
+    private String nationalID;
+    private String address;
+    private double balance;
+
+    publicAccount(String name, String phoneNumber, String nationalID, String address) {
+        this.name = name;
+        this.phoneNumber = phoneNumber;
+        this.nationalID = nationalID;
+        this.address = address;
+        this.balance = 0.0;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public String getPhoneNumber() {
+        return phoneNumber;
+    }
+
+    public String getNationalID() {
+        return nationalID;
+    }
+
+    public String getAddress() {
+        return address;
+    }
+    
+    public 
+}
